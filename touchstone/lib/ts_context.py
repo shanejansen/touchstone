caadaca12feb35c2e3fb6d943fa5868e0183d4dd
Shane Jansen <shanejjansen@gmail.com>
2021-03-13 22:31:49 -0500

    Services available should only be triggered once in develop mode. Closes #13

--- touchstone/lib/ts_context.py
+++ touchstone/lib/ts_context.py
@@ -6,10 +6,13 @@ from touchstone.lib.listeners.i_services_available_listener import IServicesAvai
 class TsContext(object):
     def __init__(self):
         self.__services_available_listeners: List[IServicesAvailableListener] = []
+        self.__triggered_services_available: bool = False
 
     def register_services_available_listener(self, listener: IServicesAvailableListener):
         self.__services_available_listeners.append(listener)
 
     def trigger_services_available(self):
-        for listener in self.__services_available_listeners:
-            listener.services_available()
+        if not self.__triggered_services_available:
+            self.__triggered_services_available = True
+            for listener in self.__services_available_listeners:
+                listener.services_available()
